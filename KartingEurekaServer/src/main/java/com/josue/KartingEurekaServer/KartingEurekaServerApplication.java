c62786e60d3e46958e477f171b1cb4903ad7799f
Josue Mateluna <josue.mateluna@usach.cl>
2025-05-28 23:31:58 -0400

    Updated config data and others

--- KartingEurekaServer/src/main/java/com/josue/KartingEurekaServer/KartingEurekaServerApplication.java
+++ KartingEurekaServer/src/main/java/com/josue/KartingEurekaServer/KartingEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.josue.KartingEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class KartingEurekaServerApplication {
 
 	public static void main(String[] args) {
