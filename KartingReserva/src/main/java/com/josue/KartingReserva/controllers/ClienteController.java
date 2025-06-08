ad4f2452da8251ffc105f6e346307a3e5a6d0c26
Josue Mateluna <josue.mateluna@usach.cl>
2025-06-08 00:41:17 -0400

    Fixed issue with CORS

--- KartingReserva/src/main/java/com/josue/KartingReserva/controllers/ClienteController.java
+++ KartingReserva/src/main/java/com/josue/KartingReserva/controllers/ClienteController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
-@CrossOrigin(origins = "*")
 public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
